ad772d41598340f402b6da4b0b6ec9c8847790ad
Fouad <f.astitou@gmail.com>
2016-10-03 16:13:14 +0200

    I have set up two collection views.

--- Scrollert/ViewController.swift
+++ Scrollert/ViewController.swift
@@ -8,16 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    @IBOutlet var horizontalCollectionView: UICollectionView!
+    @IBOutlet var verticalCollectionView: UICollectionView!
+    
+    var horizontal = [1,2,3,4,5,6,7,8,9,10]
+    var vertical = [1,2,3,4,5,6,7,8,9,10]
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        horizontalCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "HorizontalCell")
+        verticalCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "VerticalCell")
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == horizontalCollectionView {
+            return horizontal.count
+        }
+        else {
+            return vertical.count
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if collectionView == horizontalCollectionView {
+            let horizontalCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HorizontalCell", for: indexPath)
+            horizontalCell.backgroundColor = UIColor.green
+            return horizontalCell
+        }
+            
+       else {
+            
+            let verticalCell = collectionView.dequeueReusableCell(withReuseIdentifier: "VerticalCell", for: indexPath)
+            verticalCell.backgroundColor = UIColor.orange
+            return verticalCell
+        }
+    }
+    
+    // MARK: UIScrollViewDelegate
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView == self.horizontalCollectionView {
+            let horizontalScrollPosition = scrollView.contentOffset.x
+            self.verticalCollectionView.delegate = nil
+            self.verticalCollectionView.contentOffset = CGPoint(x: 0, y: horizontalScrollPosition)
+            self.verticalCollectionView.delegate = self
+        }
+        
+        if scrollView == self.verticalCollectionView {
+            let verticalScrollPosition = scrollView.contentOffset.y
+            self.horizontalCollectionView.delegate = nil
+            self.horizontalCollectionView.contentOffset = CGPoint(x: verticalScrollPosition, y: 0)
+            self.horizontalCollectionView.delegate = self
+        }
     }
 
 
