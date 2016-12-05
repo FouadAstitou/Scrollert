a096119bd31b93787851370b7e0f5a47a3200e74
Fouad <f.astitou@gmail.com>
2016-12-05 20:05:39 +0100

    Found a way to resize the center cell of the horizontal collection view. Still needs some tweaking.

--- Scrollert/ViewController.swift
+++ Scrollert/ViewController.swift
@@ -12,32 +12,34 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     @IBOutlet var horizontalCollectionView: UICollectionView!
     @IBOutlet var verticalCollectionView: UICollectionView!
     
-    var horizontal = ["1","2","3","4","5","6","7","8","9","10"]
-    var vertical = ["1","2","3","4","5","6","7","8","9","10"]
+    var horizontal = ["1","2","3","4","5","6","7","8","9","10","1","2","3","4","5","6","7","8","9","10"]
+    var horizontalCellSize = CGSize(width: 50, height: 50)
+    var horizontalLargeCellSize = CGSize(width: 80, height: 80)
 
+    
+    var centerCell = UICollectionViewCell()
+    var cellIsInCenter = false
+    var myIndexPath: IndexPath?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        horizontalCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "HorizontalCell")
-//        verticalCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "VerticalCell")
-
+    }
+    
+    // When the main view's dimensions change this will re-layout the collection view
+    override func viewWillLayoutSubviews() {
+        verticalCollectionView.collectionViewLayout.invalidateLayout()
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == horizontalCollectionView {
-            return horizontal.count
-        }
-        else {
-            return vertical.count
-        }
+        return horizontal.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         if collectionView == horizontalCollectionView {
             let horizontalCell = collectionView.dequeueReusableCell(withReuseIdentifier: "HorizontalCell", for: indexPath) as! HorizontalCell
-            horizontalCell.backgroundColor = UIColor.green
+            horizontalCell.backgroundColor = UIColor.blue
             horizontalCell.textLabel.text = horizontal[indexPath.row]
             return horizontalCell
         }
@@ -45,22 +47,17 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
        else {
             
             let verticalCell = collectionView.dequeueReusableCell(withReuseIdentifier: "VerticalCell", for: indexPath) as! VerticalCell
-            verticalCell.backgroundColor = UIColor.orange
-            verticalCell.textLabel.text = vertical[indexPath.row]
+            verticalCell.backgroundColor = UIColor.red
+            verticalCell.textLabel.text = horizontal[indexPath.row]
             return verticalCell
         }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        print("cell \(indexPath.row) selected.")
-        self.verticalCollectionView.scrollToItem(at: indexPath, at: .centeredVertically, animated: true)
     }
     
     // MARK: UIScrollViewDelegate
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if scrollView == self.horizontalCollectionView {
-            let horizontalScrollPosition = scrollView.contentOffset.x * 30
+            let horizontalScrollPosition = scrollView.contentOffset.x * 35
             self.verticalCollectionView.delegate = nil
             self.verticalCollectionView.contentOffset = CGPoint(x: 0, y: horizontalScrollPosition)
             self.verticalCollectionView.delegate = self
@@ -71,9 +68,92 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             self.horizontalCollectionView.delegate = nil
             self.horizontalCollectionView.contentOffset = CGPoint(x: verticalScrollPosition, y: 0)
             self.horizontalCollectionView.delegate = self
+            
+            
         }
+        print("scrolling")
     }
+    
 
+    // MARK: CollectionViewLayout
+    
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+//        if collectionView == verticalCollectionView {
+//            let size = CGSize(width: collectionView.bounds.width, height: collectionView.bounds.height)
+//            
+//            return size
+//        }
+//        
+//        if indexPath.item == 3 {
+//            return CGSize(width: 70, height: 70)
+//        }
+//        return CGSize(width: 50, height: 50)
+//
+//    }
+    
+    func collectionView(_ collectionView: UICollectionView,layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        if collectionView == verticalCollectionView {
+            let size = CGSize(width: collectionView.bounds.width, height: collectionView.bounds.height)
+            return size
+        }
+//        let centerIndex = self.collectionView.indexPathForItemAtPoint(self.collectionView.center)
+//        let centerIndex = self.verticalCollectionView.indexPathForItem(at: self.verticalCollectionView.center)
+//        if indexPath.item == centerIndex?.item {
+//            print(indexPath)
+//            return CGSize(width: 20, height: 70)
+//        }
+        
+        
+        if indexPath.item == self.myIndexPath?.item {
+            return CGSize(width: 70, height: 70)
+        }
+        return CGSize(width: 50, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.verticalCollectionView.scrollToItem(at: indexPath, at: .centeredVertically, animated: true)
+        print("\(indexPath)")
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        var closestCell: UICollectionViewCell = self.horizontalCollectionView.visibleCells[0]
+        for cell in self.horizontalCollectionView.visibleCells as [UICollectionViewCell] {
+            let closestCellDelta = abs(closestCell.center.x - self.horizontalCollectionView.bounds.size.width / 2.0 - self.horizontalCollectionView.contentOffset.x)
+            let cellDelta = abs(cell.center.x - self.horizontalCollectionView.bounds.size.width/2.0 - self.horizontalCollectionView.contentOffset.x)
+            if (cellDelta < closestCellDelta){
+                closestCell = cell
+                self.centerCell = cell
+                self.cellIsInCenter = true
+                horizontalCellSize = CGSize(width: 80, height: 50)
+            }
+        }
+        let indexPath = self.horizontalCollectionView.indexPath(for: closestCell)
+        self.horizontalCollectionView.scrollToItem(at: indexPath!, at: .centeredHorizontally , animated: true)
+        if closestCell.backgroundColor == UIColor.black {
+            closestCell.backgroundColor = UIColor.blue
+        }
+        horizontalCollectionView.reloadItems(at: [self.horizontalCollectionView.indexPath(for: closestCell)!])
+        closestCell.backgroundColor = UIColor.black
+        
+        // Get the center cell
+        var visibleRect = CGRect()
+        
+        visibleRect.origin = horizontalCollectionView.contentOffset
+        visibleRect.size = horizontalCollectionView.bounds.size
+        
+        let visiblePoint = CGPoint(x: visibleRect.midX, y: visibleRect.midY)
+        
+        let visibleIndexPath: IndexPath = horizontalCollectionView.indexPathForItem(at: visiblePoint)!
+        
+        print(visibleIndexPath)
+        
+        self.myIndexPath = visibleIndexPath
+        self.horizontalCollectionView.reloadData()
 
+    
+    }
 }
 
